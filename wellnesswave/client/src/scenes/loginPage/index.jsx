7cd95dd34dbb7300f6b2d86df52711ac66c44131
Aakash Gupta <aakashbb@gmail.com>
2023-10-03 22:37:35 -0400

    Final commit

---
+++ wellnesswave/client/src/scenes/loginPage/index.jsx
@@ -0,0 +1,39 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          wellnesswave
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h4" sx={{ mb: ".75rem" }}>
+          sign in.
+        </Typography>
+        <Typography fontWeight="300" variant="h6" sx={{ mb: ".5rem" }}>
+          share, connect, and heal!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
